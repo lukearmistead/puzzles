6ebf75b3ad4e3a9e9225492bd1c698884cb48d5c
Luke Armistead <armistead.luke@gmail.com>
2016-03-15 17:41:08 -0700

    profits and products

--- puzzles.py
+++ puzzles.py
@@ -19,10 +19,16 @@ def top_tweets(tweets, top):
 
 def best_knapsack(items, cap, mc_trials):
     '''
-    approximates the to include in a knapsack
+    monte carlo approximation of the items to include in a knapsack with a clever init
     takes a list of tuples containing the value and weight of an item [(1, 2), (2, 3)]
     returns the total value optimal content
     '''
+    # test case
+    # items = [(1, 3), (3, 3), (5, 3), (4, 2), (3, 2), (1, 2), (2, 2), (3, 3), (5, 5)]
+    # cap = 10
+    # mc_trials = 15 
+    # print best_knapsack(items, cap, mc_trials)
+
     best_bag = _try_knapsack(sorted(items, key=lambda item: item[0] / item[1], reverse=True))
     for iteration in xrange(mc_trials):
         shuffle(items)
@@ -48,17 +54,35 @@ def _try_knapsack(items):
     return bag_value, bag
 
 
+def max_stock_profit(prices):
+    '''
+    returns the greatest possible profit from a single long position
+    takes a chronologically ordered list of prices
+    returns the largest difference
+    '''
+    # test case
+    # prices = [10, 7, 5, 8, 11, 9]
+    top_profit = 0
+    for i, price in enumerate(prices):
+        profit = max(prices[i:]) - price
+        if profit > top_profit:
+            top_profit = profit
+    return top_profit
+         
+def product_index(entries):
+    '''
+    for each item, find the product of every other item
+    '''
+    # test case
+    # entries =  [1, 7, 3, 4]
+    products = []
+    for i, _ in enumerate(entries):
+        factors = entries[:i] + entries[i + 1:]
+        product = 1
+        for factor in factors:
+            product *= factor
+        products.append(product)
+    return products
+
 if __name__ == '__main__':
-    items = [(1, 3), (3, 3), (5, 3), (4, 2), (3, 2), (1, 2), (2, 2), (3, 3), (5, 5)]
-    cap = 10
-    mc_trials = 15 
-
-    best_bag = try_knapsack(sorted(items, key=lambda item: item[0] / item[1], reverse=True))
-    for iteration in xrange(mc_trials):
-        shuffle(items)
-        try_bag = try_knapsack(items)
-        if try_bag[0] > best_bag[0]:
-            best_bag = try_bag
-            print 'new best bag:'
-            print best_bag
-
+    pass
