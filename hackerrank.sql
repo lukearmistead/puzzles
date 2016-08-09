dc0258da754d3e16e3f3a958036eba313d9c2fc3
Luke Armistead <armistead.luke@gmail.com>
2016-08-09 12:42:01 -0700

    sql

--- hackerrank.sql
+++ hackerrank.sql
@@ -77,3 +77,22 @@ SELECT
     CONCAT("There are ",COUNT(Occupation), " ", Occupation, "s")
 FROM OCCUPATIONS
 GROUP BY Occupation;
+
+/*
+We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+
+*/
+
+SELECT CONCAT(salary * months, " ", COUNT(salary * months))
+FROM Employee
+GROUP BY salary * months
+ORDER BY salary * months DESC
+LIMIT 1;
+
+-- Another option
+select salary * months, count(*) 
+from employee 
+ where salary * months= (
+    select max(salary * months) from employee
+    )
+group by salary * months;
